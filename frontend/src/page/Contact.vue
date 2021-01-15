8a1f7fc38dae9bd798bdfbc1099042c1c3efa067
임준수-PC\junesu1104 <mkit5ain@gmail.com>
2021-01-15 18:16:37 +0900

    [추가 및 수정] 페이지 추가 및 수정

--- frontend/src/page/Contact.vue
+++ frontend/src/page/Contact.vue
@@ -1,6 +1,81 @@
 <template>
   <div id="contact">
-    contact
+    <section class="contact-wrap">
+      <div class="background-animate"></div>
+      <div class="text-wrap">
+        <div class="text">
+          <span>오</span>
+          <span>늘</span>
+          <span>도</span>
+        </div>
+        <div class="text">
+          <span>내</span>
+          <span>일</span>
+          <span>도</span>
+        </div>
+        <div class="text">
+          <span>모</span>
+          <span>레</span>
+          <span>도</span>
+        </div>
+        <div class="text">
+          <span>지</span>
+          <span>금</span>
+          <span>도</span>
+        </div>
+        <!-- <div class="border-circle-wrap">
+          <div class="circle"></div>
+          <div class="circle"></div>
+          <div class="circle"></div>
+          <div class="circle"></div>
+          <div class="circle"></div>
+        </div> -->
+      </div>
+      <div class="contact-introduction">
+        <p class="contents fade-contents">
+          <span class="initial">L</span>ike
+          <span class="initial-ass">좋아할수 있는것을</span>
+        </p> <!-- 좋아하다 -->
+        <p class="contents fade-contents">
+          <span class="initial">I</span>deas
+          <span class="initial-ass">생각하며</span>
+        </p> <!-- 생각 -->
+        <p class="contents fade-contents">
+          <span class="initial">M</span>ake
+          <span class="initial-ass">만들어요</span>
+        </p> <!-- 만든다 -->
+        <p class="contents fade-contents">
+          <span class="initial">J</span>oin
+          <span class="initial-ass">많은 프로젝트에 참여하고</span>
+        </p> <!-- 참여 -->
+        <p class="contents fade-contents">
+          <span class="initial">U</span>sually
+          <span class="initial-ass">때로는 생각하지못한 버그를 만나</span>
+        </p> <!-- 보통 -->
+        <p class="contents fade-contents">
+          <span class="initial">N</span>otion
+          <span class="initial-ass">새로운 개념을 배우며</span>
+        </p> <!-- 개념 -->
+        <p class="contents fade-contents">
+          <span class="initial">E</span>ver
+          <span class="initial-ass">항상 기억하고 고민하여</span>
+        </p> <!-- 언제나 -->
+        <p class="contents fade-contents">
+          <span class="initial">S</span>hiny
+          <span class="initial-ass">늘 새롭고 완벽을 추구하는</span>
+        </p> <!-- 빛나는 -->
+        <p class="contents fade-contents">
+          <span class="initial">U</span>nderline
+          <span class="initial-ass">결과물을 만들어냅니다.</span>
+        </p>
+      </div>
+      <div>
+        <p>함께할 준비 되셨나요?</p>
+        <p>임준수 . 31</p>
+        <p><a href="tel:01055580087"></a>010 . 5558 . 0087</p>
+        <p><a href="mailto:mkit5ain@naver.com">mkit5ain@naver.com</a></p>
+      </div>
+    </section>
   </div>
 </template>
 <script>
---
+++ frontend/src/page/Experience.vue
@@ -0,0 +1,91 @@
+<template>
+  <div id="experience">
+    <section class="work-wrap">
+      <div class="background-animate"></div>
+      <div id="work" class="category-contents active">
+        <div class="symbol-animation type-c">
+          <div class="symbol-wrap">
+            <div class="symbol-01"></div>
+            <div class="symbol-02"></div>
+            <div class="symbol-03"></div>
+            <div class="symbol-04"></div>
+          </div>
+        </div>
+        <div class="symbol-animation type-d">
+          <div class="symbol-wrap">
+            <div class="symbol-01"></div>
+            <div class="symbol-02"></div>
+            <div class="symbol-03"></div>
+            <div class="symbol-04"></div>
+          </div>
+        </div>
+        <ul class="work-list">
+          <li class="list-item">
+            <figure class="item">
+              <a href="#"><img src="~@/assets/images/project/pickdata.png" alt=""></a>
+            </figure>
+          </li>
+          <li class="list-item">
+            <figure class="item">
+              <a href="#"><img src="~@/assets/images/project/bigcial.png" alt=""></a>
+            </figure>
+          </li>
+          <li class="list-item">
+            <figure class="item">
+              <a href="#"><img src="~@/assets/images/project/greencar.png" alt=""></a>
+            </figure>
+          </li>
+          <li class="list-item">
+            <figure class="item">
+              <a href="#"><img src="~@/assets/images/project/goodirect.png" alt=""></a>
+            </figure>
+          </li>
+          <li class="list-item">
+            <figure class="item">
+              <a href="#"><img src="~@/assets/images/project/lotto.png" alt=""></a>
+            </figure>
+          </li>
+          <li class="list-item">
+            <figure class="item">
+              <a href="#"><img src="~@/assets/images/project/moblo.png" alt=""></a>
+            </figure>
+          </li>
+          <li class="list-item">
+            <figure class="item">
+              <a href="#"><img src="~@/assets/images/project/swarovski.png" alt=""></a>
+            </figure>
+          </li>
+          <li class="list-item">
+            <figure class="item">
+              <a href="project/mulawear.html"><img src="~@/assets/images/project/mula.png" alt=""></a>
+            </figure>
+          </li>
+          <li class="list-item">
+            <figure class="item">
+              <a href="#"><img src="~@/assets/images/project/veracity.png" alt=""></a>
+            </figure>
+          </li>
+          <li class="list-item">
+            <figure class="item">
+              <a href="#"><img src="~@/assets/images/project/swmp.png" alt=""></a>
+            </figure>
+          </li>
+          <li class="list-item">
+            <figure class="item">
+              <a href="#"><img src="~@/assets/images/project/ulvine.png" alt=""></a>
+            </figure>
+          </li>
+          <li class="list-item">
+            <figure class="item">
+              <a href="#"><img src="~@/assets/images/project/hojeon.png" alt=""></a>
+            </figure>
+          </li>
+        </ul>
+      </div>
+    </section>
+  </div>
+</template>
+<script>
+
+</script>
+<style lang="scss"></style>
