5cf5edadb268c0ce7708f43a21f48230d5814627
임준수-PC\junesu1104 <mkit5ain@gmail.com>
2021-01-15 17:34:27 +0900

    [수정] 라우터 추가 및 콤포넌트 노출

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,27 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <ui-header/>
+      <ui-aside/>
+      <ui-footer/>
     </div>
     <router-view/>
   </div>
 </template>
+<script>
+  import Header from '@/components/Header.vue'
+  import Aside from '@/components/Aside.vue'
+  import Footer from '@/components/Footer.vue'
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+  export default {
+    name: 'App',
+    components: {
+      'ui-header': Header,
+      'ui-aside': Aside,
+      'ui-footer': Footer
     }
   }
-}
+</script>
+<style lang="scss">
+  @import 'assets/sass/main.scss'
 </style>
