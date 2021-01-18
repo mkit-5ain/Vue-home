fdee1d08c7d1105561ba66f3e82eeadded0e0a88
임준수-PC\junesu1104 <mkit5ain@gmail.com>
2021-01-18 18:18:34 +0900

    [수정] aside,footer 주석

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,23 +2,23 @@
   <div id="app">
     <div id="nav">
       <ui-header/>
-      <ui-aside/>
-      <ui-footer/>
+      <!-- <ui-aside/> -->
+      <!-- <ui-footer/> -->
     </div>
     <router-view/>
   </div>
 </template>
 <script>
   import Header from '@/components/Header.vue'
-  import Aside from '@/components/Aside.vue'
-  import Footer from '@/components/Footer.vue'
+  // import Aside from '@/components/Aside.vue'
+  // import Footer from '@/components/Footer.vue'
 
   export default {
     name: 'App',
     components: {
       'ui-header': Header,
-      'ui-aside': Aside,
-      'ui-footer': Footer
+      // 'ui-aside': Aside,
+      // 'ui-footer': Footer
     }
   }
 </script>
